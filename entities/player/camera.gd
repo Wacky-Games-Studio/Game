52bb46d12e872edb365b8cab762f325c9f95d319
Flillip <119044858+Flillip@users.noreply.github.com>
2023-10-23 22:28:13 +0200

    redid camera smoothing

--- entities/player/camera.gd
+++ entities/player/camera.gd
@@ -1,14 +1,16 @@
 extends Camera2D
 
+@export var move_speed: float = 1
+
 @onready var notifier: VisibleOnScreenNotifier2D = $"../OnScreenNotifier"
 @onready var player: CharacterBody2D = $".."
 
 func _ready():
-	position_smoothing_enabled = false
+	#position_smoothing_enabled = false
 	var viewport_size = get_viewport_rect().size
 	global_position = viewport_size / 2
-	await get_tree().create_timer(0.1).timeout
-	position_smoothing_enabled = true
+	#await get_tree().create_timer(0.1).timeout
+	#position_smoothing_enabled = true
 
 func _on_screen_notifier_screen_exited():
 	var viewport_size = get_viewport_rect().size.x
@@ -16,4 +18,8 @@ func _on_screen_notifier_screen_exited():
 	   global_position.x - viewport_size / 2 < player.position.x:
 		return
 
-	global_position.x += viewport_size if global_position < player.position else viewport_size * -1
+	#global_position.x += viewport_size if global_position < player.position else viewport_size * -1
+	var final_val = global_position.x + (viewport_size if global_position < player.position else viewport_size * -1)
+	
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "global_position:x", final_val, move_speed)
