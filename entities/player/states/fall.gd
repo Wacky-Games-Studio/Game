53909cf5983e0aa4c687136ef363a278214a46bc
Flillip <119044858+Flillip@users.noreply.github.com>
2023-10-30 13:19:44 +0100

    added slide fall animation

--- entities/player/states/fall.gd
+++ entities/player/states/fall.gd
@@ -3,6 +3,7 @@ extends State
 @export var move_state: State
 @export var idle_state: State
 @export var jump_state: State
+@export var slide_fall_animation_name: String
 
 @onready var coyote_timer: Timer = %CoyoteTimer
 
@@ -27,7 +28,10 @@ func process_physics(delta: float) -> State:
 	if parent.is_on_wall_only() and \
 		((parent.get_wall_normal().x < 0 and Input.is_action_pressed("walk_right")) or \
 		(parent.get_wall_normal().x > 0 and Input.is_action_pressed("walk_left"))):
+		parent.animator.play(slide_fall_animation_name)
 		parent.velocity.y = min(parent.velocity.y, parent.wall_slide_gravity)
+	elif parent.animator.current_animation != animation_name:
+		parent.animator.play(animation_name)
 	
 	if dir != 0:
 		parent.velocity.x = lerp(parent.velocity.x, dir * parent.speed, parent.acceleration)
