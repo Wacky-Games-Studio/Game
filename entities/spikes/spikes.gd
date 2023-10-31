34bcc2ea7932d61fe3749fe5a2ebcafd2152ee28
Flillip <119044858+Flillip@users.noreply.github.com>
2023-10-31 12:42:52 +0100

    added spike rotation

--- entities/spikes/spikes.gd
+++ entities/spikes/spikes.gd
@@ -1,4 +1,10 @@
 extends Area2D
 
+var dir = Vector2.UP
+
+func _ready():
+	var angle_to_rotate = dir.angle_to(Vector2.UP)
+	rotate(angle_to_rotate)
+
 func _on_body_entered(body):
 	print("AAAAAAAAAAAAAA")
--- scenes/dungeon/assets/tile_map.gd
+++ scenes/dungeon/assets/tile_map.gd
@@ -12,22 +12,11 @@ func add_spikes():
 			var tile := get_cell_tile_data(2, Vector2i(x, y))
 			if tile == null: continue
 			
-			var tile_data: Vector2 = tile.get_custom_data("direction")
+			var tile_dir: Vector2 = tile.get_custom_data("direction")
 			
 			var instance = spikes.duplicate().instantiate()
+			instance.dir = tile_dir
 			instance.global_position = to_global(map_to_local(Vector2(x, y)))
 			add_child(instance)
 			
 			set_cell(2, Vector2i(x, y))
-
-			
-#			match tile_dir:
-#				Vector2(-1,0):
-#
-
-			#if tile_id == tile_id_to_replace:
-			#	var instance = packed_scene.instance()
-			#	instance.position = map_to_world(Vector2(x, y))
-			#	get_tree().get_root().add_child(instance)  # Add the packed scene to the root of the scene tree
-			#	tilemap.set_cell(x, y, -1)  # Remove the original tile by setting it to -1
-
