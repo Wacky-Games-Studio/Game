3f3009dd34434fbc89b0313f1603014fe72b6318
Flillip <119044858+Flillip@users.noreply.github.com>
2023-11-13 20:40:03 +0100

    updated spring

--- entities/spring/spring.gd
+++ entities/spring/spring.gd
@@ -1,7 +1,10 @@
 class_name Spring
 extends Area2D
 
-@export var shoot_velocity = 500
+@export var jump_height: float = 100
+@export var jump_time_to_peak: float = .5
+
+@onready var jump_velocity := (( 2.0 * jump_height) / jump_time_to_peak) * -1.0
 
 var custom_data
 var dir := Vector2.UP
@@ -11,11 +14,11 @@ func _ready() -> void:
 	var angle_to_rotate = dir.angle_to(Vector2.UP)
 	rotate(angle_to_rotate)
 	
-	if dir.x != 0: shoot_velocity *= -1
+	if dir.x != 0: jump_velocity *= -1
 
 func _on_body_entered(body) -> void:
 	if body is Player:
 		$AnimationPlayer.play("boing")
 		$Sound.play()
-		body.velocity = shoot_velocity * dir.normalized()
+		body.velocity = jump_velocity * dir.normalized()
 		body.spring_jump()
