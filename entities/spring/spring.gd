ab96b62b2390fc514b302dc5ae5de5ddf0cee2b9
Flillip <119044858+Flillip@users.noreply.github.com>
2023-11-18 22:20:06 +0100

    updated spring so that side wall jumps add vertical velocity and animations

--- entities/spring/spring.gd
+++ entities/spring/spring.gd
@@ -2,9 +2,11 @@ class_name Spring
 extends Area2D
 
 @export var jump_height: float = 100
+@export var side_spring_jump_height: float = 50
 @export var jump_time_to_peak: float = .5
 
 @onready var jump_velocity := (( 2.0 * jump_height) / jump_time_to_peak)
+@onready var side_spring_jump_velocity := (( 2.0 * side_spring_jump_height) / jump_time_to_peak)
 
 var custom_data
 var dir := Vector2.UP
@@ -14,11 +16,16 @@ func _ready() -> void:
 	var angle_to_rotate = dir.angle_to(Vector2.UP)
 	rotate(angle_to_rotate)
 	
-	if dir.x != 0: jump_velocity *= -1
+	if dir.x != 0: 
+		jump_velocity *= -1
+		side_spring_jump_velocity *= -1
 
 func _on_body_entered(body) -> void:
 	if body is Player:
 		$AnimationPlayer.play("boing")
 		$Sound.play()
 		body.velocity = jump_velocity * dir.normalized()
+		if dir.x != 0:
+			body.velocity.y = side_spring_jump_velocity
+		
 		body.spring_jump()
