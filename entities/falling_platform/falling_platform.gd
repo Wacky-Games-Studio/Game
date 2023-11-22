7fd7e1c14b9541f4d671eeca734035f224f1051a
Flillip <filip.holmgren2@elev.ga.lbs.se>
2023-11-22 13:50:31 +0100

    more work on falling platform

---
+++ entities/falling_platform/falling_platform.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var gravity = 100
+
+func _physics_process(delta):
+	move_and_slide()
+
+func _on_body_entered(body):
+	if body.velocity.y > 0:
+		return
+	
+	$AnimationPlayer.play("fall")
+	
+	await $AnimationPlayer.animation_finished
+	
+	#velocity.y = gravity
