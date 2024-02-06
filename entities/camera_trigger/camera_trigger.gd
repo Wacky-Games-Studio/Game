1ef8f0c089295da7376e648fea0eb93a16ba0b53
Flillip <filip.holmgren2@elev.ga.lbs.se>
2024-02-06 15:25:39 +0100

    updating camera_trigger to add verticallity

--- entities/camera_trigger/camera_trigger.gd
+++ entities/camera_trigger/camera_trigger.gd
@@ -1,27 +1,43 @@
 @icon("res://icons/trigger.svg")
 extends Area2D
 
-@export var enable_static_camera_on_left_side := true
+@export var static_side := StaticSide.Left
 
 var entered_left: bool
+var entered_top: bool
 
 func _on_body_entered(body):
 	if body is Player:
-		var player_pos: float = body.global_position.x
-		var pos := global_position.x
-		entered_left = player_pos < pos
+		var player_pos: Vector2 = body.global_position
+		var pos := global_position
+		entered_left = player_pos.x < pos.x
+		entered_top = player_pos.y < pos.y
 
 func _on_body_exited(body):
 	if body is Player:
-		var player_pos: float = body.global_position.x
-		var pos := global_position.x
-		var exited_left = player_pos < pos
+		var player_pos: Vector2 = body.global_position
+		var pos := global_position
+		
+		#horizontal
+		var exited_left = player_pos.x < pos.x
 		var exited_right = not exited_left
 		var entered_right = not entered_left
 		
-		if entered_left:
-			if exited_left: body.set_static(enable_static_camera_on_left_side)
-			elif exited_right: body.set_static(not enable_static_camera_on_left_side)
-		elif entered_right:
-			if exited_left: body.set_static(enable_static_camera_on_left_side)
-			elif exited_right: body.set_static(not enable_static_camera_on_left_side)
+		# vertical
+		var exited_top = entered_top
+		var exited_bottom = player_pos.y < pos.y
+		var entered_bottom = not entered_left
+		
+		if static_side in [StaticSide.Left, StaticSide.Right]:
+			if entered_left:
+				if exited_left: body.set_static(static_side == StaticSide.Left)
+				elif exited_right: body.set_static(static_side != StaticSide.Left)
+			elif entered_right:
+				if exited_left: body.set_static(static_side == StaticSide.Left)
+				elif exited_right: body.set_static(static_side != StaticSide.Left)
+		elif static_side in [StaticSide.Top, StaticSide.Down]:
+			pass
+
+enum StaticSide {
+	Top, Down, Left, Right
+}
