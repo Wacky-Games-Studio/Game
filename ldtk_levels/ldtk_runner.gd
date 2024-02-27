cbca75c34c26ecd787df2e47a8816c33dea523f2
Flillip <filip.holmgren2@elev.ga.lbs.se>
2024-02-27 13:54:52 +0100

    added git ignore and updated runner script

--- ldtk_levels/ldtk_runner.gd
+++ ldtk_levels/ldtk_runner.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-const level1 = preload("res://ldtk_levels/worlds/Level_1.tscn")
+@export var level: String = "1"
+#const level1 = preload("res://ldtk_levels/worlds/Level_1.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_child(level1.instantiate())
+	add_child(load("res://ldtk_levels/worlds/Level_" + level + ".tscn").instantiate())
 
