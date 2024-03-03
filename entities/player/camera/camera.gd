e5224ef77128073d2bdb97d06646faa899f44296
Flillip <119044858+Flillip@users.noreply.github.com>
2024-03-03 20:26:26 +0100

    added new dynamic camera

--- entities/player/camera/camera.gd
+++ entities/player/camera/camera.gd
@@ -3,39 +3,100 @@ extends Camera2D
 @export var move_speed: float = .8
 @export var instant_move: bool = false
 
+@export_category("Dynamic camera")
+@export var target_distance := 16.0
+@export var camera_speed := 100.0
+@export var buffer_size := 32.0
+
 @onready var notifier: VisibleOnScreenNotifier2D = $"../OnScreenNotifier"
 @onready var player: CharacterBody2D = $".."
 @onready var screen_size: Vector2 = get_viewport().get_visible_rect().size
 
 var _locked := false
 var _is_static := true
-var _target_distance := 100
-var _camera_speed := 100
 var _camera_lockers: Array[CameraLocker] = []
 var _queued_left: int
 var _queued_top: int
 var _queued_right: int
 var _queued_bottom: int
+var _lerp_target: float
+var _should_lerp: bool
+var _start_lerp_position: float
+var _lerp_timer: float
+var _current_state: DynamicState = DynamicState.MovingRight
+
+enum DynamicState {
+	MovingRight,
+	MovingLeft,
+	None
+}
 
 func _ready() -> void:
 	set_static(_is_static)
 	update_position()
+	#Engine.time_scale = 0.1
 
-func _process(delta):
+func _physics_process(delta: float) -> void:
 	if _is_static: return
 	
-	#var path: Path2D = get_node("/root/Level_1/Path2D")
-	#global_position = path.curve.get_closest_point(player.global_position - path.global_position) + path.global_position
+	var center_position: float = get_screen_center_position().x
+	var buffer_max: float = center_position + target_distance + buffer_size
+	var buffer_min: float = center_position - target_distance - buffer_size
 	
-	var new_offset: float = global_position.x
-	if player.velocity.x > 0:
-		var target = (player.position.x + _target_distance)
-		new_offset = min(new_offset + _camera_speed * delta, target)
-	elif player.velocity.x < 0:
-		var target = (player.position.x - _target_distance)
-		new_offset = max(new_offset - _camera_speed * delta, target)
+	if player.global_position.x > buffer_max and player.velocity.x > 0 and _should_lerp == false:
+		_start_lerp(target_distance, DynamicState.MovingRight)
 
-	global_position.x = new_offset
+	elif player.global_position.x < buffer_min and player.velocity.x < 0 and _should_lerp == false:
+		_start_lerp(-target_distance, DynamicState.MovingLeft)
+	
+	if _should_lerp:
+		_update_lerp_position(delta)
+		return
+
+	_update_global_position()
+
+func _start_lerp(target: float, state: DynamicState) -> void:
+	_lerp_target = target
+	_should_lerp = true
+	_start_lerp_position = global_position.x
+	_lerp_timer = 0.0
+	_current_state = state
+
+func _update_lerp_position(delta: float) -> void:
+	var target = player.global_position.x + _lerp_target
+		
+	_lerp_timer += camera_speed * delta
+	global_position.x = lerpf(_start_lerp_position, target, _lerp_timer)
+	if _lerp_timer >= 1.0:
+		_should_lerp = false
+
+func _update_global_position() -> void:
+	if _current_state == DynamicState.MovingRight and player.velocity.x > 0 and player.global_position.x > global_position.x - target_distance:
+		global_position.x = player.global_position.x + target_distance
+	elif _current_state == DynamicState.MovingLeft and player.velocity.x < 0 and player.global_position.x < global_position.x + target_distance:
+		global_position.x = player.global_position.x - target_distance
+	
+func _process(_delta):
+	queue_redraw()
+
+func _draw():
+	if not get_tree().debug_collisions_hint:
+		return
+
+	#var new_offset: float = global_position.x
+	var center: float = get_screen_center_position().x
+	var player_buffer_max: float = target_distance + center + buffer_size
+	var player_buffer_min: float = -target_distance + center - buffer_size
+	#var player_position_diff: float = center - player.global_position.x
+
+	
+	draw_rect(Rect2(to_local(Vector2(player_buffer_min, 0)), Vector2(2, 272)), Color.BLUE)
+	draw_rect(Rect2(to_local(Vector2(player_buffer_max, 0)), Vector2(2, 272)), Color.GREEN)
+
+	# middle
+	draw_line(to_local(Vector2(center, 0)), to_local(Vector2(center, 272)), Color.RED, 2);
+	draw_line(to_local(Vector2(center - target_distance, 0)), to_local(Vector2(center - target_distance, 272)), Color.PINK, 2)
+	draw_line(to_local(Vector2(center + target_distance, 0)), to_local(Vector2(center + target_distance, 272)), Color.YELLOW, 2)
 
 func update_position() -> void:	
 	if _is_static:
@@ -66,6 +127,7 @@ func _on_screen_notifier_screen_exited() -> void:
 	
 	var target_pos = _calculate_new_pos()
 	
+	# HACK: what???
 	if target_pos == global_position:
 		var viewport_size := get_viewport_rect().size
 		player.global_position.y -= viewport_size.y
@@ -94,18 +156,19 @@ func set_static(value: bool) -> void:
 	
 	#drag_horizontal_enabled = _is_static == false
 	#drag_vertical_enabled = _is_static == false
-	top_level = _is_static == true
+	#top_level = _is_static == true
 	
 	global_position = original_pos
 	
 	if _is_static == false:
-		var tween = get_tree().create_tween()
-		#tween.tween_property(self, "position", Vector2(0,0), 0.5)
-		tween.tween_method(_move_camera, position, Vector2(0,0), 0.5)
 		limit_right  = _queued_right
 		limit_left   = _queued_left
 		limit_top    = _queued_top
 		limit_bottom = _queued_bottom
+
+		# if   player.velocity.x > 0: _start_lerp(target_distance, DynamicState.MovingRight)
+		# elif player.velocity.x < 0: _start_lerp(-target_distance, DynamicState.MovingLeft)
+
 	else:
 		var target_pos = _calculate_new_pos()
 		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE)
