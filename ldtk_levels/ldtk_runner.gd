530b934abfc4b1f485afee8a09fe335f245b019c
Flillip <filip.holmgren2@elev.ga.lbs.se>
2024-03-18 12:29:11 +0100

    fixed dumb bug

--- ldtk_levels/ldtk_runner.gd
+++ ldtk_levels/ldtk_runner.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @export var level: String = "1"
-const level1 = preload("res://ldtk_levels/worlds/Level_1.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
