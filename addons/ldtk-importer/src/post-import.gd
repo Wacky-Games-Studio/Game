4dc416dcf0625f7ed567d327f5a74b1ced59e8bd
Okiman42 <antonaxel05@gmail.com>
2024-03-19 14:48:50 +0100

    anton sync

--- addons/ldtk-importer/src/post-import.gd
+++ addons/ldtk-importer/src/post-import.gd
@@ -8,7 +8,7 @@ static func run(element: Variant, script_path: String) -> Variant:
 		if not script or not script is GDScript:
 			printerr("Post-Import: '%s' is not a GDScript" % [script_path])
 			return ERR_INVALID_PARAMETER
-
+		
 		script = script.new()
 		if not script.has_method("post_import"):
 			printerr("Post-Import: '%s' does not have a post_import() method" % [script_path])
