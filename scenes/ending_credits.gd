0665caa1e5792b567c9390f4ca16809d6bc56f91
Flillip <filip.holmgren2@elev.ga.lbs.se>
2024-04-05 15:05:15 +0200

    updated credits and splash

--- scenes/ending_credits.gd
+++ scenes/ending_credits.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 @export var speed = 10
+@export var top_y = -300
 
-@onready var canvas_layer = $CanvasLayer
+@onready var canvas_layer = $ScrollingCanvas
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	canvas_layer.offset.y -= speed * delta
 
-	if Input.is_action_pressed("ui_cancel"):
+	if Input.is_action_pressed("ui_cancel") or canvas_layer.offset.y <= top_y:
 		get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
--- scenes/splash.gd
+++ scenes/splash.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+func freeze():
+    get_tree().paused = true
+
+func defrozt():
+    get_tree().paused = false
